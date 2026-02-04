2d5dbb2413e8900ef6dc946be47591010cce5374
Chris Keith <chris.keith@gmail.com>
2026-02-03 19:38:54 -0800

    Use # to mark end of each classmate

--- raw_to_csv.py
+++ raw_to_csv.py
@@ -8,24 +8,39 @@ def log(message):
     print(str(datetime.datetime.now()) + '\t'+ script_name + ': ' + message)
 
 class Extractor:
+
+    classmates = []
    
-   def main(self):
+    def addClassmate(self, lines):
+        classmate = {}
+        classmate["name"] = lines[0]
+        for line in lines[1:]:
+            if re.match(r'^\S+@\S+\.\S+$', line):
+                classmate["email"] = line
+            elif re.match(r'^\+?\d[\d -]{7,}\d$', line):
+                classmate["phone"] = line
+            else:
+                if not classmate.get("info"):
+                    classmate["info"] = line
+                else:
+                    classmate["info"] += " " + line
+        self.classmates.append(classmate)
+
+    def main(self):
         fn = "./classmates.txt"
         with open(fn, 'r', encoding='utf-8') as f:
             lines = f.readlines()
         log(f"Read {len(lines)} lines from {fn}")
-        i = 0
-        skipCount = 0
-        lines2 = []
+        cmLines = []
         for line in lines:
-            if (i % 2) == 0:
-                lines2.append(line)
-            else:
-                if line.strip() != "":
-                    log(f"Skipping line: {i}:{line.strip()}")
-                    skipCount += 1
-            i += 1
-        log(f"Filtered to {len(lines2)} lines, skipped {skipCount} lines")
+            line = line.strip()
+            if line:
+                if line[0] == '#':
+                    self.addClassmate(cmLines)
+                    cmLines = []
+                else:
+                    cmLines.append(line)
+        log(f"classmates found: {len(self.classmates)}")
 
 if '__main__' == __name__:
     Extractor().main()
